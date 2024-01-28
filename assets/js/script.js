7202a42c37d69dd58461b71e6e8deb412ef3560d
fireraindi <fireraindy@gmail.com>
2024-01-28 23:30:49 +0700

    menambahkan js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('-ml-[1000px]');
+});
+
+// Klik di luar hamburger
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('-ml-[1000px]');
+  }
+});
