5cec26103d73bc52efedd716c2324d7804067bc2
fireraindi <fireraindy@gmail.com>
2024-01-28 21:58:37 +0700

    menambahkan file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./view/**/*.{html,js}"],
+  theme: {
+   
+    extend: {
+      fontFamily:{
+        'rubik': ['Rubik Doodle Shadow','system-ui'],
+      },
+      
+    },
+  },
+  plugins: [],
+
+}
+
