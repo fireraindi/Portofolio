a6216380e67bbe136bb17133871081301050538d
fireraindi <fireraindy@gmail.com>
2024-01-29 19:26:40 +0700

    membetulkan slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,13 @@ const navMenu = document.querySelector('#nav-menu');
 
 hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
-  navMenu.classList.toggle('-ml-[1000px]');
+  navMenu.classList.toggle('-translate-x-full');
 });
 
 // Klik di luar hamburger
 window.addEventListener('click', function (e) {
   if (e.target != hamburger && e.target != navMenu) {
     hamburger.classList.remove('hamburger-active');
-    navMenu.classList.add('-ml-[1000px]');
+    navMenu.classList.add('-translate-x-full');
   }
 });
